e56be281fac92bc2f2eca846d0b12d0c0a2705eb
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-06-08 03:02:01 +0530

    Updated the Majority Sol 2

--- Day3Array/MajorityElement2.cpp
+++ Day3Array/MajorityElement2.cpp
@@ -24,3 +24,43 @@ public:
         return ans;
     }
 };
+
+// Updated Solution
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+        int e1(INT_MIN),e2(INT_MAX),cnt1{},cnt2{};
+
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=e2){
+                e1=nums[i];
+                cnt1++;
+            }else if(cnt2==0 && nums[i]!=e1){
+                e2=nums[i];
+                cnt2++;
+            }else if(nums[i]==e1){
+                cnt1++;
+            }else if(nums[i]==e2){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cout<<e1<<' '<<e2;
+
+        cnt1=0,cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==e1)cnt1++;
+            else if(nums[i]==e2)cnt2++;
+        }
+        int k=nums.size()/3;
+        vector<int>ans;
+        if(cnt1>k)ans.push_back(e1);
+        if(cnt2>k)ans.push_back(e2);
+
+        return ans;
+    }
+};
