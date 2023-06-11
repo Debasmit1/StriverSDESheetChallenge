17b93937a0e68ce087de45cdabc1d97a1cfb29a1
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-06-12 01:54:27 +0530

    Added Middle Link List Solution

---
+++ Day1LinkList/MiddleLL.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(!head->next)return head;
+        ListNode *slow=head,*fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
