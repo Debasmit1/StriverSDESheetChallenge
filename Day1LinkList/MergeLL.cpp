d93f963f6d5063bcbcf7e0e21330bda78c10b7e7
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-06-12 20:45:12 +0530

    Added recursion solution

--- Day1LinkList/MergeLL.cpp
+++ Day1LinkList/MergeLL.cpp
@@ -22,3 +22,28 @@ public:
         return dmyPtr.next;
     }
 };
+
+// Recursion
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *res;
+        res=NULL;
+
+        if(!list2){
+            return list1;
+        }else if(!list1){
+            return list2;
+        }
+
+        if(list1->val<=list2->val){
+            res=list1;
+            res->next=mergeTwoLists(list1->next,list2);
+        }else{
+            res=list2;
+            res->next=mergeTwoLists(list1,list2->next);
+        }
+        return res;
+    }
+};
