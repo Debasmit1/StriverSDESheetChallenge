4c06a04017b173eb3bab8c7c2106cadbc4a1b3fd
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-06-15 13:50:39 +0530

    Added updated Sol

--- Day3LinkList/CopyList.cpp
+++ Day3LinkList/CopyList.cpp
@@ -27,3 +27,35 @@ public:
     return mp[head];
     }
 };
+
+// Updated Solution
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head )return head;
+        Node *temp=head;
+        while(temp){
+            Node *copy=new Node(temp->val);
+            copy->next=temp->next;
+            temp->next=copy;
+            temp=copy->next;
+        }
+        temp=head;
+        while(temp){
+            if(temp->random)
+            temp->next->random=temp->random->next;
+            temp=temp->next->next;
+        }
+        Node dmyNode(0);
+        dmyNode.next=head->next;
+        Node *oldNode=head,*newNode=head->next;
+        while(oldNode){
+            oldNode->next = newNode->next;
+            if(newNode->next)newNode->next=newNode->next->next;
+            oldNode=oldNode->next;
+            newNode=newNode->next;
+        }
+        return dmyNode.next;
+    }
+};
